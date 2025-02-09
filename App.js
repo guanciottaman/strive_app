87247f544edb8e3a9648c883112577bd0d538e84
guanciottaman <italia.matteo00@gmail.com>
2025-02-09 18:50:44 +0100

    First commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import 'expo-router/entry-classic';
+
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello, World!</Text>
+    </View>
+  );
+}
